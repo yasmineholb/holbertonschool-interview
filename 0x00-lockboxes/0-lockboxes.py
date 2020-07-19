8b7c1f1b7bdfc058e3169ca27d1c0fe21eecaf52
yasmineholb <1055@holbertonschool.com>
2020-07-19 06:10:46 -0500

    lock

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -11,37 +11,28 @@ def tests(boxes):
         return 0
     elif boxes[0] == [] or boxes[0] == [0]:
         return 0
-    else:
-        for i in range(len(boxes)):
-            for j in range(len(boxes[i])):
-                cles.append(boxes[i][j])
-        cles = set(cles)
-        if 0 in cles:
-            cles.pop()
-        cles = list(cles)
-        if max(cles) > len(boxes)-1:
-            b = 0
-        if len(cles) < len(boxes)-1:
-            b = 0
-        return(b)
 
 
-def iterative_dfs(graph, start):
+def iterative_dfs(graph, start, m):
+    """ function """
     stack, path = [start], []
     while stack:
         vertex = stack.pop()
         if vertex in path:
             continue
-        path.append(vertex)
-        for neighbor in graph[vertex]:
-            stack.append(neighbor)
+        if vertex < m:
+            path.append(vertex)
+            for neighbor in graph[vertex]:
+                stack.append(neighbor)
     return path
 
 
 def canUnlockAll(boxes):
+    """function """
     b = tests(boxes)
+    m = len(boxes)
     if b == 0:
         return False
     else:
         graph = {i: boxes[i] for i in range(len(boxes))}
-        return len(iterative_dfs(graph, 0)) == len(boxes)
+        return len(iterative_dfs(graph, 0, m)) == len(boxes)
