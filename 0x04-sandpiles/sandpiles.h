510cf70a875d3cc8650eb062a8af0cee4db587e1
yasmineholb <1055@holbertonschool.com>
2020-08-17 17:31:58 +0200

    sand

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef sandp
+#define sandp
+#include <stdlib.h>
+#include <stdio.h>
+static void print_grid(int grid[3][3]);
+int check(int grid1[3][3]);
+void copy(int p[3][3], int s[3][3]);
+static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+#endif
