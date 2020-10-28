273d39c7c2e4554af9dc889a04aa1a9caa62251a
yasmine hamdi <55131741+yasmineholb@users.noreply.github.com>
2020-10-28 22:33:59 +0100

    Update 0-nqueens.py

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -2,7 +2,7 @@
 """ nqueens """
 import sys
 
-
+#Number of queens
 com = sys.argv[1]
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
@@ -12,24 +12,72 @@ if type(int(com)) is not int:
 if int(sys.argv[1]) < 4:
     print("N must be at least 4")
     exit(1)
-p = []
-for i in range(int(com)):
-    p1 = []
-    for j in range(int(com)):
-        p1.append(0)
-    p.append(p1)
-print(p)
-    
-def isSafe(board, row, col):
-    """ men geeks for geeks"""
-    for i in range(col): 
-        if board[row][i] == 1: 
-            return False
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)): 
-        if board[i][j] == 1: 
-            return False
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)): 
-        if board[i][j] == 1: 
-            return False
-    return True
 
+N = int(com)
+
+
+board = [[0 for i in range(N)]for j in range(N)]
+
+k = 1
+ 
+def printSolution(board): 
+ 
+    global k
+    k = k + 1
+    S = []
+    for i in range(N): 
+        for j in range(N):
+            if board[i][j] == 1:
+                S.append([i, j])
+    print(S)
+ 
+
+def isSafe(board, row, col) :
+     
+    for i in range(col): 
+        if (board[row][i]): 
+            return False
+ 
+    i = row
+    j = col
+    while i >= 0 and j >= 0:
+        if(board[i][j]):
+            return False;
+        i -= 1
+        j -= 1
+ 
+    i = row
+    j = col
+    while j >= 0 and i < N:
+        if(board[i][j]):
+            return False
+        i = i + 1
+        j = j - 1
+ 
+    return True
+ 
+
+def solveNQUtil(board, col) :
+    if (col == N): 
+        printSolution(board) 
+        return True
+    res = False
+    for i in range(N):
+
+        if (isSafe(board, i, col)): 
+            board[i][col] = 1; 
+            res = solveNQUtil(board, col + 1) or res; 
+            board[i][col] = 0 
+    return res
+ 
+
+def solveNQ() :
+    board = [[0 for j in range(N)] 
+                for i in range(N)]
+    if (solveNQUtil(board, 0) == False):    
+        print("Solution does not exist") 
+        return
+    return
+ 
+solveNQ() 
+ 
