4c6c2f159a79ced2be60d6881b71bf33cb42d702
yasmineholb <1055@holbertonschool.com>
2021-04-03 14:58:33 +0200

    perimeter island function

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+island_perimeter module
+"""
+
+
+def island_perimeter(grid):
+    """ Function that returns the perimeter
+        of the island described in grid """
+    m = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i-1][j] != 1:
+                    m += 1
+                if i == len(grid) - 1 or grid[i+1][j] != 1:
+                    m += 1
+                if j == 0 or grid[i][j-1] != 1:
+                    m += 1
+                if j == len(grid[i]) - 1 or grid[i][j+1] != 1:
+                    m += 1
+    return (m)
